5ebd876d04db79f251611c3c4cbe0903c9e01c9e
matigonza <gonzalezmatias879@gmail.com>
2024-07-10 22:29:22 -0300

    creacion de proyecto

--- src/main/java/com/example/Proyect/LiterAlura/LiterAluraApplication.java
+++ src/main/java/com/example/Proyect/LiterAlura/LiterAluraApplication.java
@@ -1,13 +1,26 @@
 package com.example.Proyect.LiterAlura;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.Proyect.LiterAlura.principal.Principal;
+import com.example.Proyect.LiterAlura.repository.AutorRepository;
 
 @SpringBootApplication
-public class LiterAluraApplication {
+public class LiterAluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private AutorRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repository);
+		principal.mostrarMenu();
+	}
 }
